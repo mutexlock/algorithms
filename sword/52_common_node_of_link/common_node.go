90713043e1a3ef4fcdecb2c56356305c21184056
lizhongwei <lizhongwei1985@gmail.com>
2020-04-30 15:27:50 +0800

    init

---
+++ sword/52_common_node_of_link/common_node.go
@@ -0,0 +1,10 @@
+package _2_common_node_of_link
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	return headA
+}
---
+++ sword/52_common_node_of_link/common_node_test.go
@@ -0,0 +1,13 @@
+package _2_common_node_of_link
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_OK(t *testing.T) {
+	ast := assert.New(t)
+	ast.Equal("1", "1", "11")
+
+}
