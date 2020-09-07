9550874e25b4dcedce7e97aa5dc6c3df337d73fe
lizhongwei <lizhongwei1985@gmail.com>
2020-09-07 19:27:39 +0800

    sword

---
+++ sword/010_fib/fib.go
@@ -0,0 +1,18 @@
+package _10_fib
+
+func fib(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	var (
+		a    = 0
+		b    = 1
+		temp int
+	)
+	for i := 2; i <= n; i++ {
+		temp = b
+		b = (a + b) % 1000000007
+		a = temp
+	}
+	return b
+}
