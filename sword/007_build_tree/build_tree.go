c396996d17ad0fdde2f230f4c134607ee207d65a
lizhongwei <lizhongwei1985@gmail.com>
2020-09-07 18:49:57 +0800

    sword

--- sword/007_build_tree/build_tree.go
+++ sword/007_build_tree/build_tree.go
@@ -2,6 +2,27 @@ package _07_build_tree
 
 import "github.com/mutexlock/algorithms/common"
 
+//输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+//
+//
+//
+//例如，给出
+//
+//前序遍历 preorder = [3,9,20,15,7]
+//中序遍历 inorder = [9,3,15,20,7]
+//返回如下的二叉树：
+//
+//3
+/// \
+//9  20
+///  \
+//15   7
+//
+//
+//限制：
+//
+//0 <= 节点个数 <= 5000
+
 type TreeNode = common.TreeNode
 
 func buildTree(preorder []int, inorder []int) *TreeNode {
